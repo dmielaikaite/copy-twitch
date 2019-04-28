64d024184d42de2a4d3d1b7f1ee10151a6d24512
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-04-28 15:31:09 +0200

    working with portal for modal

--- client/src/components/streams/streamDelete.js
+++ client/src/components/streams/streamDelete.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+import Modal from '../modal';
+
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+
+  const actions = (
+    <div>
+      <button className="ui negative button"> Delete </button>
+      <button className="ui button"> Cancel </button>
+    </div>
+  )
+
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete stream"
+        content="are you sure?"
+        actions={actions}
+      />
+    </div>
+  )
 }
 
 export default StreamDelete;
